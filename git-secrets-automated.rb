23194b5d46ef82fc6d0cf4ef68b54d7072cf4751
Kartik <kartik@hyperverge.co>
2020-10-16 01:57:05 +0530

    Add installation script

---
+++ git-secrets-automated.rb
@@ -0,0 +1,12 @@
+class GitSecretsAutomated < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/kartik1998/git-secrets-automated/archive/v1.0.0.tar.gz"
+  sha256 "dc31b17e20d9e47d882d840f81aea92826a0da156220a4a904cfd4b7d253cb45"
+  license ""
+
+  def install
+        bin.install "git-secrets-automated"
+  end
+
+end
